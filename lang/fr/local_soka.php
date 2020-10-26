5dca9c62efb7cc1e1e387ec97016e885d379c56d
Laurent David <lmedavid@gmail.com>
2020-10-26 17:56:16 +0100

    Add french language strings

---
+++ lang/fr/local_soka.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Soka project.
+ *
+ *
+ * @package    local_soka
+ * @copyright 2020 - CALL Learning - Laurent David <laurent@call-learning>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Outil Soka';
+$string['sokageneralsettings'] = 'Réglages principaux Outil Soka';
+$string['sokamanagement'] = 'Gestion Outil Soka';
+$string['softskillsbadge'] = 'Badge pour l\'activité Softskills';
+$string['softskillsbadge_help'] = 'Badge à émettre lors de la fin de l\'activité Softskills';
+$string['softskillsbadge:emailsubject'] = 'Votre badge Softskill Experience est prêt!';
+$string['softskillsbadge:emailfooter'] = 'Merci d \' avoir utilisé le test de softskill!';
+$string['softskillsbadge:emaillink'] = 'Veuillez suivre ce lien pour obtenir votre badge.';
+$string['softskillsbadge:emailbody'] = 'Vous avez rempli le questionnaire Softskill sur {$ a-> sitename}
+ et nous sommes heureux de vous livrer </p> <p> Votre badge d’expérience </p> <p> {$ a-> scoreisplay} </p';
+$string['yourscore'] = 'Votre score de réussite atteint est {$ a}';
+$string['mustbesameorigin'] = 'Doit être de la même origine pour accepter la requête';
